0808c0e5bdcb7d4e14bd205a6d815b4295387283
Murshid P <murshidpgv@gmail.com>
2023-10-24 16:00:41 +0530

    07reactrouter code added

--- 07reactrouter/src/main.jsx
+++ 07reactrouter/src/main.jsx
@@ -6,22 +6,43 @@ import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } 
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
 import User from './components/User/User.jsx'
-import Github, {githubInfoLoader} from './components/Github/Github.jsx'
+import Github, { githubInfoLoader } from './components/Github/Github.jsx'
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout/>,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />
+//       },
+//       {
+//         path: "about",
+//         element: <About />
+//       },
+//       {
+//         path: "contact",
+//         element: <Contact />
+//       }
+//     ]
+//   }
+// ])
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout />}>
-      <Route path='' element={<Home />}/>
+      <Route path='' element={<Home />} />
       <Route path='about' element={<About />} />
-      <Route path='user/' element={<User />} >
-        <Route path=':userid' element={<User />} />
-      </Route>
+      <Route path='contact' element={<Contact />} />
+      <Route path='user/:userid' element={<User />} />
       <Route 
       loader={githubInfoLoader}
       path='github' 
-      element={<Github />} />
-      <Route path='*' element={<div>Not Found</div>} />
+      element={<Github />}
+       />
     </Route>
   )
 )
